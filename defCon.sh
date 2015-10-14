6cf24a0732b93e5f557daf640fe229ccc8a33f81
Tomas Trnka <tomas@trnkatomas.eu>
2015-10-14 20:19:27 +0200

    solved problem with symlinks

--- defCon.sh
+++ defCon.sh
@@ -1,1 +1,89 @@
-../defCon.sh
+#!/usr/bin/env bash
+#*****************************************************************#
+#*  Monuting defCon images
+#*  author: Tomas Trnka         email:trnkato2@fel.cvut.cz       
+#*****************************************************************#
+
+if [ $# -lt 3 ]; then
+	echo "You must specify at least 3 parameters" >&2
+	echo "Try ${0} -h for more informations" >&2
+	exit 1
+fi
+
+#initializing flags
+v=""
+m="/mnt/defCon"
+p=""
+u=0
+
+#processing input agruments using getopts
+while getopts "hv:m:p:u" opt; do
+  case $opt in
+	v)
+			v="$OPTARG";;
+	m)
+			m="$OPTARG";;
+	p)
+			p="$OPTARG";;
+	u)
+			u=1;;
+	h) 
+			echo "Usage: ${0} -p file_with_passwords -v first_crypto_volume [-m mount_point] "
+			echo "commands:"
+			echo "-v	the file with cryptographic volume"
+			echo "-m	mount point, default set to /mnt/defCon"
+			echo "-p	file with passwords, passwords should be each on a new line"
+			echo "-u    unmounts all mounted volumes"
+			echo "-h	this help..."
+			exit 1;;
+	esac
+done
+
+prefix="Round"
+
+if [ $u -gt 0 ]; then
+	echo "Unmounting all mounted volumes"
+	truecrypt -d	
+	exit
+fi
+
+#solve the problem with file names with whitespaces
+SAVEIFS=$IFS
+IFS=$(echo -en "\n\b")
+if [ -f $p ]; then
+	readarray a < $p
+	counter=1
+
+	for item in "${a[@]}"; do
+		#echo $item
+
+		# Test whether moutn point exists
+		if ! [ -d "${m}/Round${counter}" ]; then
+				sudo mkdir -p "${m}/Round${counter}"
+				echo "Creating mountpoint ${m}/Round${counter}"
+		fi
+
+		# Mount parent folder
+		if [ -f $v ];  then
+			truecrypt --password=$item $v "${m}/Round${counter}"
+			echo "Mounting volume $v at ${m}/Round${counter}"
+			v="${m}/Round1/Contest/"			
+		fi
+		# Mount consecutive rounds
+		if [ -d $v ] && [ $counter -gt 1 ] ; then
+			truecrypt --password=$item "${v}/Round${counter}.tc" "${m}/Round${counter}"
+			echo "Mounting volume ${v}${counter}.tc at ${m}/Round${counter}"	
+		fi
+
+		let counter=counter+1
+	done
+else
+	echo "No file with passwords found. Program will exit"
+fi
+
+IFS=$SAVEIFS
+exit
+
+
+
+
--- imgThumbnailer.sh
+++ imgThumbnailer.sh
@@ -1,1 +1,94 @@
-../imgThumbnailer.sh
+#!/usr/bin/env bash
+#*****************************************************************#
+#*  Shell thumbnailing the images
+#*  author: Tomas Trnka         email:trnkato2@fel.cvut.cz       
+#*****************************************************************#
+
+if [ $# -lt 1 ]; then
+	echo "${0} : you must specify at least one parameter" >&2
+	echo "Try ${0} --help, -h for more informations" >&2
+	exit 1
+fi
+
+#initializing flags
+f=0
+d=0
+s=300
+
+#processing input agruments using getopts
+while getopts "hf:d:s:qwn:" opt; do
+  case $opt in
+	f)
+			f="$OPTARG";;
+	n)
+			n="$OPTARG";;
+	s)
+			s="$OPTARG";;
+	q)
+			q=true;;	
+	w)
+			w=true;;
+	d)
+			d="$OPTARG";;
+  	h) 
+			echo "Usage: ${0} [-f tar file] [optional command]"
+			echo "commands:"
+			echo "-s	set the bigger size of thumbnail"
+			echo "-d	set the folder you would like to thumbnail"
+			echo "-f	set the file to thumbnail"
+			echo "-q	set the thumbnail to square"
+			echo "-w	compress quality to web (75%)"
+			exit 1;;
+	esac
+done
+
+#solve the problem with file names with whitespaces
+SAVEIFS=$IFS
+IFS=$(echo -en "\n\b")
+if [ -f $f ]; then
+	echo "Thumbnailing file $i ...."
+	if [ $n ]; then
+		filename=$(basename "$f")
+		extension="${filename##*.}"
+		filename="${filename%.*}"
+		new_name=$n.$extension
+	else
+		new_name="thumb_${f}"
+	fi
+	convert -thumbnail $s $f $new_name #thumb_${f}
+fi
+
+if [ -d $d ];  then
+	cd $d
+	out=$(echo Today is $(date))
+	echo $out
+	files="""$(ls * | egrep -i ".jp(e?)g$|.png$|.gif$")"""
+	#echo $files
+	for i in $files; do
+		echo "Thumbnailing file $i ...."
+		#filename="${i%.*}"
+		#extension="${i##*.}"
+		#echo $filename $extension
+		#convert -thumbnail $s $i thumb_$i
+		if [ $q ]; then
+			convert $i -thumbnail ${s}x${s}^ -gravity center -extent ${s}x${s} thumb_${i}
+		else
+			convert -thumbnail $s $i thumb_${i}
+			if [ `identify -format '%[exif:orientation]' $i` -eq 6 ]; then
+				convert -rotate 90 thumb_${i} thumb_${i}
+			fi
+            if [ `identify -format '%[exif:orientation]' $i` -eq 8 ]; then
+				convert -rotate -90 thumb_${i} thumb_${i}
+			fi
+			if [ $w ]; then
+				imgWeb -f thumb_${i} -q 75
+			fi
+		fi
+	done
+fi
+
+echo "##########################################################"
+IFS=$SAVEIFS
+
+
+
